6f8f5d754dd68a35044c36cad66ffb6c1e32bf1c
LimaCL <113440100+LimaCL@users.noreply.github.com>
2022-09-30 11:58:13 -0300

    NgModule Part2
    Continuação
    NgModule curso Angular

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 
 
 @NgModule({
   declarations: [
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     CommonModule,
