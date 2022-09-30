24e09d5139b52f88845c754241a83ce79baeec25
LimaCL <113440100+LimaCL@users.noreply.github.com>
2022-09-30 13:40:30 -0300

    Templates
    Sobre Templates Curso Angular

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[]  = ['Java', 'Angular', 'Python'];
+
+
+  constructor() {
+      this.nomePortal = 'http://loiane.training';
+
+      for (let i=0; i > this.cursos.length; i++){
+        let cursos = this.cursos[i];
+      }
+   }
 
   ngOnInit(): void {
   }
