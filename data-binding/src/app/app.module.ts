d4d4964be3f00094553eb29c38c8d0eb7fc61256
LimaCL <113440100+LimaCL@users.noreply.github.com>
2022-10-03 10:49:22 -0300

    Class e Style Binding
    Aprendizado sobre Class e Style Binding em Angular

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { DataBinding2Component } from './data-binding2/data-binding2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    DataBinding2Component
   ],
   imports: [
     BrowserModule
