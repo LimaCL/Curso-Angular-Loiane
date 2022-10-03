41a781ecd800695be4e840e2db0d163b412904fa
LimaCL <113440100+LimaCL@users.noreply.github.com>
2022-10-03 10:11:26 -0300

    Data-Binding
    Aprendizado sobre Data-Binding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://google.com';
+  cursoAngular: boolean = true;
+  urlImage = 'http://lorempixel.com.br/500/400/?1';
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
