f75c419ae3ff26252463567ab9c628b6af86718a
LimaCL <113440100+LimaCL@users.noreply.github.com>
2022-10-03 15:42:27 -0300

    Event Binding
    Aprendizado sobre Event Binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { DataBinding2Component } from './data-binding2/data-binding2.component';
+import { DataBinding3Component } from './data-binding3/data-binding3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
-    DataBinding2Component
+    DataBinding2Component,
+    DataBinding3Component
   ],
   imports: [
     BrowserModule
