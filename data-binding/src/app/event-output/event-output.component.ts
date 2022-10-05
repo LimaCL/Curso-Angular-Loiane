82a21c158a3f53e7b784cf5ce2dcbe82c03754a5
LimaCL <113440100+LimaCL@users.noreply.github.com>
2022-10-05 14:12:49 -0300

    Acessando Elemento DOM no template com ViewChild
    Praticando Acessando Elemento DOM no template com ViewChild

--- data-binding/src/app/event-output/event-output.component.ts
+++ data-binding/src/app/event-output/event-output.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -9,7 +9,10 @@ export class EventOutputComponent implements OnInit {
   
   valor: number = 0;
 
+  @ViewChild('campoInput') campoValorInput!: HTMLElement;
+
   incrementa() {
+    console.log(this.campoValorInput);
     this.valor++;
   }
 
