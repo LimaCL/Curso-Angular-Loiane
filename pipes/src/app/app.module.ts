67a6c97d9ce6d0356f55fa4548cc9ad2b2b1a332
LimaCL <113440100+LimaCL@users.noreply.github.com>
2022-10-17 16:29:47 -0300

    Locale Pipe

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -14,7 +14,12 @@ import { CamelCasePipe } from './camel-case.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
