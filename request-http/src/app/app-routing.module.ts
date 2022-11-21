365de1f1b3baecf41070850bbb3e0455396c4c54
LimaCL <113440100+LimaCL@users.noreply.github.com>
2022-11-21 09:40:08 -0300

    Continuação Modulo Request HTTP

--- request-http/src/app/app-routing.module.ts
+++ request-http/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { HomeComponent } from './../../../rotas/src/app/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full',  redirectTo: 'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(cursos => cursos.CursosModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
