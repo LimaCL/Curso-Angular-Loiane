e0a8ecdac103b9fd51e51ba244d8386bbd2fc811
LimaCL <113440100+LimaCL@users.noreply.github.com>
2022-11-24 15:39:07 -0300

    Criando Mensagem de Erro com BootStrap e começo do forms de atualização

--- request-http/src/app/app.module.ts
+++ request-http/src/app/app.module.ts
@@ -2,24 +2,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CursosModule } from './cursos/cursos.module';
+import { UnsubscribeRxjsModule } from './unsubscribe-rxjs/unsubscribe-rxjs.module';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { PocBaseComponent } from './unsubscribe-rxjs/poc-base/poc-base.component';
+import { SharedModule } from './shared/shared.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    PocBaseComponent,
-  ],
+    HomeComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CursosModule,
-    HttpClientModule
+    HttpClientModule,
+    UnsubscribeRxjsModule,
+    ModalModule.forRoot(),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- request-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ request-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,13 @@
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { empty, Observable, Subject } from 'rxjs';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
+import { catchError } from 'rxjs';
+import { AlertModalComponent } from 'src/app/shared/alert-modal/alert-modal.component';
+import { AlertModalService } from 'src/app/shared/alert-modal.service';
+
+
 
 @Component({
   selector: 'app-cursos-lista',
@@ -15,12 +21,33 @@ export class CursosListaComponent implements OnInit {
 
   cursos$!: Observable<Curso[]>;
 
-  constructor(private service: CursosService) { }
+  error$ = new Subject<boolean>();
+  bsModalRef?: BsModalRef;
+
+  constructor(private service: CursosService,
+    //private modalService: BsModalService
+    private alertService: AlertModalService) { }
 
   ngOnInit(): void {
    // this.service.list().subscribe( dados => this.cursos = dados);
+  this.onRefresh();
 
-   this.cursos$ = this.service.list();
+
+}
+
+  onRefresh(){
+    this.cursos$ = this.service.list()
+   .pipe(
+    catchError(error => {
+      console.error(error);
+      //this.error$.next(true);
+      this.handleError();
+      return empty();
+    })
+   );
   }
 
+  handleError(){
+    this.alertService.showAlertDanger('Erro ao carregar cursos, Tente Novamente mais tarde');
+  }
 }
